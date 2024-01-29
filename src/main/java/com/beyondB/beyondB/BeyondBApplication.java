676cd1136b98c3d38d9b6d5f24ed0c7cdc04feab
koreanair777 <jinuklee777@soongsil.ac.kr>
2024-01-29 15:18:14 +0900

    :rocket: Initial Commit

---
+++ src/main/java/com/beyondB/beyondB/BeyondBApplication.java
@@ -0,0 +1,13 @@
+package com.beyondB.beyondB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beyondB/beyondB/BeyondBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyondB.beyondB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
