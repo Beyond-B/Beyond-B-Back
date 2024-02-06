907da6bc5ebd47f92b385c6145face151874aee7
koreanair777 <jinuklee777@soongsil.ac.kr>
2024-02-06 22:33:27 +0900

    :ambulance: [HOTFIX]: 한글문제가 아니라 엔티티 설계 문제 해결

--- src/main/java/com/beyondB/beyondB/entity/Diary.java
+++ src/main/java/com/beyondB/beyondB/entity/Diary.java
@@ -1,6 +1,5 @@
 package com.beyondB.beyondB.entity;
 
-import com.beyondB.beyondB.entity.mapping.DiaryFeeling;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -8,11 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import java.time.LocalDate;
 import lombok.*;
-
-import java.util.Date;
 
 @Entity
 @Builder
@@ -38,14 +34,13 @@ public class Diary {
 
     private LocalDate date;
 
-    @OneToOne
-    private DiaryFeeling diaryFeeling;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feeling_id")
+    private Feeling feeling;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    public void setDiaryFeeling(DiaryFeeling diaryFeeling) {
-        this.diaryFeeling = diaryFeeling;
-    }
+
 }
--- src/main/java/com/beyondB/beyondB/repository/DiaryFeelingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.beyondB.beyondB.repository;
-
-import com.beyondB.beyondB.entity.mapping.DiaryFeeling;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaryFeelingRepository extends JpaRepository<DiaryFeeling, Long> {
-}
