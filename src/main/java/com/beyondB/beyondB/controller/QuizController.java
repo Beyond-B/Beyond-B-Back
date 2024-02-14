d4533c53acda5e3171cbb1664eed4dbd50b75056
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-02-15 01:54:28 +0900

    ✨ [Feat]: 책 퀴즈 제출 API

--- src/main/java/com/beyondB/beyondB/controller/QuizController.java
+++ src/main/java/com/beyondB/beyondB/controller/QuizController.java
@@ -1,16 +1,23 @@
 package com.beyondB.beyondB.controller;
 
 import com.beyondB.beyondB.apiPayload.BaseResponse;
+import com.beyondB.beyondB.converter.BookConverter;
 import com.beyondB.beyondB.converter.QuizConverter;
+import com.beyondB.beyondB.dto.request.BookRequestDTO;
+import com.beyondB.beyondB.dto.request.QuizRequestDTO;
+import com.beyondB.beyondB.dto.response.BookResponseDTO;
 import com.beyondB.beyondB.dto.response.QuizResponseDTO;
+import com.beyondB.beyondB.entity.Book;
 import com.beyondB.beyondB.entity.BookQuiz;
 import com.beyondB.beyondB.entity.User;
+import com.beyondB.beyondB.entity.mapping.UserBook;
 import com.beyondB.beyondB.security.handler.annotation.AuthUser;
 import com.beyondB.beyondB.service.QuizService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,5 +36,16 @@ public class QuizController {
         BookQuiz requestedQuiz= quizService.getQuiz(user, bookId);
 
         return BaseResponse.onSuccess(QuizConverter.toQuizDetailDTO(requestedQuiz));
+    }
+    @ApiResponses({@ApiResponse(responseCode = "COMMON200", description = "등록 성공")})
+    @Operation(summary = "퀴즈 제출", description = "퀴즈 제출 API입니다.")
+    @PostMapping("/submit")
+    @Parameter(name = "user", hidden = true)
+    public BaseResponse<Void> submitQuiz(
+            @AuthUser User user,
+            @Valid@RequestBody QuizRequestDTO.SubmitQuizDTO request) {
+        quizService.submitQuiz(user, request);
+
+        return BaseResponse.onSuccess(null);
     }
 }
--- src/main/java/com/beyondB/beyondB/service/QuizService.java
+++ src/main/java/com/beyondB/beyondB/service/QuizService.java
@@ -1,8 +1,10 @@
 package com.beyondB.beyondB.service;
 
+import com.beyondB.beyondB.dto.request.QuizRequestDTO;
 import com.beyondB.beyondB.entity.BookQuiz;
 import com.beyondB.beyondB.entity.User;
 
 public interface QuizService {
     BookQuiz getQuiz(User user, Long bookId);
+    void submitQuiz(User user, QuizRequestDTO.SubmitQuizDTO request);
 }
